b200d3c4f1a4b357184a27102c6b6df0fddbfa42
Abiodun <aabiodunawoyemi@gmail.com>
2022-09-06 20:57:21 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// CONTRACT ADDRESS: 0x057c50505c000805e2F7dcAD0E5deabC2Fd977Cc
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-12">
+      <div className="">
+
+
+        <div className="text-center">
+          <h1 className="text-3xl font-medium">TASK MANAGER</h1>
+          <p className="text-lg">Your daily task manager that helps you to keep track of your project!</p>
+        </div>
+      </div>
     </div>
   );
 }
